cd86c59583c414cc5f6953d49d48acb1b8354410
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-19 18:10:52 -0300

    [master] feat: Reducer to call use cases for associations sheet requests

--- src/store/associations/reducers.js
+++ src/store/associations/reducers.js
@@ -1,6 +1,15 @@
 import associations from './store';
 
-const funcs = {};
+// Events
+import associationsRequested from './use_cases/associationsRequested';
+import associationsReceived from './use_cases/associationsReceived';
+import associationsRequestFailed from './use_cases/associationsRequestFailed';
+
+const funcs = {
+	'[associations] FETCH_REQUESTED': associationsRequested,
+	'[associations] FETCH_RECEIVED': associationsReceived,
+	'[associations] FETCH_REQUEST_FAILED': associationsRequestFailed,
+};
 
 const reducer = (state = associations, action) => {
 	const new_state = funcs[action.type] !== undefined
