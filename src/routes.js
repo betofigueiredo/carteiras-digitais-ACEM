eadfde3ef6c010b25602b012d281fac702a71d1b
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-19 16:04:37 -0300

    [master] fix: Change route for tests

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Pages
+import ChooseAssociation from './pages/ChooseAssociation';
 import Page1 from './pages/Page1';
 
 const Routes = () => (
 	<Switch>
 		{/* <Route path="/associado/:id" exact component={Associado} /> */}
 		{/* <Route path="/validar/:id" exact component={Validar} /> */}
-		<Route path="/" exact component={Page1} />
+		<Route path="/" exact component={ChooseAssociation} />
 		<Route path="*" exact component={Page1} />
 	</Switch>
 );
