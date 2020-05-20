02401c6b102ef79bbb01ad86ddb659b9e48a01bb
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-20 15:36:31 -0300

    [master] feat: Root page component handling url params and basic navigation

--- src/pages/AllPages/AllPages.js
+++ src/pages/AllPages/AllPages.js
@@ -1,25 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 // CSS
 import * as s from './AllPages.style';
-
-// TODO
-// Photo by eberhard grossgasteiger on Unsplash
 
 // Components
 import AssociationsSearch from '../../components/AssociationsSearch';
 import MembersSearch from '../../components/MembersSearch';
 import MemberProfile from '../../components/MemberProfile';
 
-const AllPages = () => {
+// Functions
+import { urlUtils } from '../../functions/urlUtils';
+
+const AllPages = ({ match, history }) => {
 	const [page, setPage] = useState(1);
+	const page_url_param = urlUtils.getPageFromUrl(match);
+
+	useEffect(() => {
+		setPage(page_url_param);
+	}, [page_url_param]);
+
+	function moveToPage(to) {
+		const page_url = urlUtils.getUrlFromPage(to);
+		history.push(page_url);
+	}
 
 	return (
 		<s.Wrapper>
 			<s.Pages>
 				<AssociationsSearch
 					page={page}
-					setPage={setPage}
+					setPage={moveToPage}
 				/>
 				<MembersSearch
 					page={page}
@@ -32,6 +43,17 @@ const AllPages = () => {
 			</s.Pages>
 		</s.Wrapper>
 	);
+};
+
+AllPages.propTypes = {
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			page: PropTypes.string,
+		}),
+	}).isRequired,
+	history: PropTypes.shape({
+		push: PropTypes.func,
+	}).isRequired,
 };
 
 export default AllPages;
