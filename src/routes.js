20f8213aeaa97334c3c4c0062ecf2df83f505655
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-20 15:36:02 -0300

    [master] feat: Route accepting page param to improve navigation

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,10 @@ import { Route, Switch } from 'react-router-dom';
 
 // Pages
 import AllPages from './pages/AllPages';
-// import Page1 from './pages/Page1';
 
 const Routes = () => (
 	<Switch>
-		{/* <Route path="/associado/:id" exact component={Associado} /> */}
-		{/* <Route path="/validar/:id" exact component={Validar} /> */}
-		<Route path="/" exact component={AllPages} />
+		<Route path="/:page" exact component={AllPages} />
 		<Route path="*" exact component={AllPages} />
 	</Switch>
 );
