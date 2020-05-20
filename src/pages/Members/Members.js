686ad899d83c45bdaa03d0505d1db205abfe7532
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-20 20:49:30 -0300

    [master] refactor: Members insides pages folder

--- src/pages/Members/Members.js
+++ src/pages/Members/Members.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 // CSS
-import { Button } from 'antd';
-import * as s from './MembersSearch.style';
+import { Button, Input } from 'antd';
+import * as s from './Members.style';
 
 // Functions
 import { checkPagePosition } from '../../functions/checkPagePosition';
 
-const MembersSearch = ({ page, moveToPage }) => {
+const Members = ({ page, moveToPage }) => {
 	const position = checkPagePosition({
 		actual_page: 2,
 		selected_page: page,
 	});
 
+	useEffect(() => {
+		if (position === 'active') {
+			console.log('aaaaaaaaaaaaaaa');
+		}
+	}, [position]);
+
 	return (
 		<s.MembersPage position={position}>
-			MembersSearch
+			<Button onClick={() => moveToPage(1)}>Voltar</Button>
 			<br /><br />
-			<Button type="primary" onClick={() => moveToPage(1)}>1</Button>
+			Members
+			<br /><br />
+			<Input placeholder="Basic usage" />
 			<br /><br />
 			<Button type="primary" onClick={() => moveToPage(3)}>3</Button>
 		</s.MembersPage>
 	);
 };
 
-MembersSearch.propTypes = {
+Members.propTypes = {
 	page: PropTypes.number.isRequired,
 	moveToPage: PropTypes.func.isRequired,
 };
 
-export default MembersSearch;
+export default Members;
