d9721763cbe8eacab2d713377b27c7ef8465d6fe
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-19 22:02:24 -0300

    [master] feat: Basic routes

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Pages
-import ChooseAssociation from './pages/ChooseAssociation';
-import Page1 from './pages/Page1';
+import AllPages from './pages/AllPages';
+// import Page1 from './pages/Page1';
 
 const Routes = () => (
 	<Switch>
 		{/* <Route path="/associado/:id" exact component={Associado} /> */}
 		{/* <Route path="/validar/:id" exact component={Validar} /> */}
-		<Route path="/" exact component={ChooseAssociation} />
-		<Route path="*" exact component={Page1} />
+		<Route path="/" exact component={AllPages} />
+		<Route path="*" exact component={AllPages} />
 	</Switch>
 );
 
