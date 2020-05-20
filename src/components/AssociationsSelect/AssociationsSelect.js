7ee3741d2ac95b748a4c84b48e27866f313fcfdd
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-20 20:56:24 -0300

    [master] feat: Select to choose associations

---
+++ src/components/AssociationsSelect/AssociationsSelect.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+
+// CSS
+import { Select } from 'antd';
+
+// Actions
+import { associationSelected } from '../../store/associations/actions';
+
+const AssociationsSelect = () => {
+	const dispatch = useDispatch();
+	const associations = useSelector(store => store
+		.associations, shallowEqual) || {};
+	const fetching = associations.fetching || '';
+	const all_ids = associations.all_ids || [];
+	const by_id = associations.by_id || {};
+	const style = { width: '300px' };
+
+	function handleSelect(association_id) {
+		const action = associationSelected({
+			association_id,
+		});
+		dispatch(action);
+	}
+
+	const loading = (
+		fetching === 'TO_START'
+		|| fetching === 'FETCHING'
+	);
+	if (loading) {
+		return (
+			<Select style={style} loading />
+		);
+	}
+
+	return (
+		<Select onChange={handleSelect} style={style}>
+			{all_ids.map(association_id => (
+				<Select.Option
+					key={association_id}
+					value={association_id}
+				>
+					{by_id[association_id].associacao}
+				</Select.Option>
+			))}
+		</Select>
+	);
+};
+
+export default AssociationsSelect;
