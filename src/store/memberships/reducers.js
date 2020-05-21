610ca07d85e2fcdcf42add662608d054992de979
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-21 13:53:08 -0300

    [master] feat: Reducer for members events

--- src/store/memberships/reducers.js
+++ src/store/memberships/reducers.js
@@ -1,6 +1,17 @@
 import memberships from './store';
 
-const funcs = {};
+// Events
+import storeCleared from './use_cases/storeCleared';
+import membershipsRequested from './use_cases/membershipsRequested';
+import membershipsReceived from './use_cases/membershipsReceived';
+import membershipsRequestFailed from './use_cases/membershipsRequestFailed';
+
+const funcs = {
+	'[shared] ASSOCIATION_SELECTED': storeCleared,
+	'[memberships] FETCH_REQUESTED': membershipsRequested,
+	'[memberships] FETCH_RECEIVED': membershipsReceived,
+	'[memberships] FETCH_REQUEST_FAILED': membershipsRequestFailed,
+};
 
 const reducer = (state = memberships, action) => {
 	const new_state = funcs[action.type] !== undefined
