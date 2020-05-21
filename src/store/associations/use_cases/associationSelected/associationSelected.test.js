779908cd4f978557ea8a9a5370a1532ab64d4c2f
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-21 13:52:39 -0300

    [master] test: Test case for associationSelected function

---
+++ src/store/associations/use_cases/associationSelected/associationSelected.test.js
@@ -0,0 +1,27 @@
+import associationSelected from './associationSelected';
+import associations from '../../store';
+
+describe('associationSelected', () => {
+	it('should not break with undefined values', () => {
+		const result = associationSelected();
+		expect(result).toStrictEqual({});
+	});
+
+	it('should handle null value', () => {
+        const state = { ...associations };
+        const action = { association_id: null };
+        const result = associationSelected(state, action);
+		expect(result).toStrictEqual(state);
+	});
+
+	it('should select the association', () => {
+        const state = { ...associations };
+        const action = { association_id: '78645435656' };
+        const result = associationSelected(state, action);
+        const expected = {
+            ...state,
+            selected_association_id: '78645435656',
+        }
+		expect(result).toStrictEqual(expected);
+	});
+});
