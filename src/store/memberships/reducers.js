c9a074a639f983ed0f9b750e7dea3ef7250039be
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-21 19:24:41 -0300

    [master] feat: Reducers to handle search and select member

--- src/store/memberships/reducers.js
+++ src/store/memberships/reducers.js
@@ -5,12 +5,18 @@ import storeCleared from './use_cases/storeCleared';
 import membershipsRequested from './use_cases/membershipsRequested';
 import membershipsReceived from './use_cases/membershipsReceived';
 import membershipsRequestFailed from './use_cases/membershipsRequestFailed';
+import searchInputChanged from './use_cases/searchInputChanged';
+import memberSelected from './use_cases/memberSelected';
+import memberIdCleared from './use_cases/memberIdCleared';
 
 const funcs = {
 	'[shared] ASSOCIATION_SELECTED': storeCleared,
 	'[memberships] FETCH_REQUESTED': membershipsRequested,
 	'[memberships] FETCH_RECEIVED': membershipsReceived,
 	'[memberships] FETCH_REQUEST_FAILED': membershipsRequestFailed,
+	'[memberships] SEARCH_INPUT_CHANGE': searchInputChanged,
+	'[memberships] MEMBER_SELECTED': memberSelected,
+	'[memberships] MEMBER_ID_CLEARED': memberIdCleared,
 };
 
 const reducer = (state = memberships, action) => {
