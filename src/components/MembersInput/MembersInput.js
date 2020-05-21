2f1b2ee1e67bf278c683adc3a5d515c514905990
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-21 19:20:30 -0300

    [master] feat: Input to search for members

---
+++ src/components/MembersInput/MembersInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+// CSS
+import { Input } from 'antd';
+
+// Actions
+import {
+	searchInputChanged,
+	memberSelected,
+} from '../../store/memberships/actions';
+
+const MembersInput = () => {
+	const dispatch = useDispatch();
+
+	function handleSearch(e) {
+		const value = e?.target?.value || '';
+		const action = searchInputChanged({ value });
+		dispatch(action);
+	}
+
+	function selectMember() {
+		const action = memberSelected();
+		dispatch(action);
+	}
+
+	return (
+		<Input
+			onChange={handleSearch}
+			onPressEnter={selectMember}
+		/>
+	);
+};
+
+export default MembersInput;
