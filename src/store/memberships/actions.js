66f57cec0f65ba99dac0bc784f7934cef4a0fb5d
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-21 19:24:26 -0300

    [master] feat: Actions to handle search and select member

--- src/store/memberships/actions.js
+++ src/store/memberships/actions.js
@@ -1,3 +1,16 @@
 export const membersSheetRequested = () => ({
 	type: '[memberships] FETCH_REQUESTED',
 });
+
+export const searchInputChanged = ({ value }) => ({
+	type: '[memberships] SEARCH_INPUT_CHANGE',
+	value,
+});
+
+export const memberSelected = () => ({
+	type: '[memberships] MEMBER_SELECTED',
+});
+
+export const memberIdCleared = () => ({
+	type: '[memberships] MEMBER_ID_CLEARED',
+});
