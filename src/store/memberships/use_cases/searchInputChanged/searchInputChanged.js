fc7711434a59f63025596c0a0901f97e47701ae7
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-21 19:28:00 -0300

    [master] feat: Use case to handle member search

---
+++ src/store/memberships/use_cases/searchInputChanged/searchInputChanged.js
@@ -0,0 +1,22 @@
+const memberSelected = (state = {}, action = {}) => {
+	const updated_state = { ...state };
+
+	function updateStore() {
+		return updated_state;
+	}
+
+	function clearSelectedMember() {
+		updated_state.selected_member_id = null;
+		return updateStore();
+	}
+
+	function updateSearchValue() {
+		const value = action.value || '';
+		updated_state.search = value;
+		return clearSelectedMember();
+	}
+
+	return updateSearchValue();
+};
+
+export default memberSelected;
