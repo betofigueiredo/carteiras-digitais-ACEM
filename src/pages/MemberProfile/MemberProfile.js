8582751555223fd78d38bb8669e83f39fbcaf350
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-22 16:09:03 -0300

    [master] feat: Profile page components organized

--- src/pages/MemberProfile/MemberProfile.js
+++ src/pages/MemberProfile/MemberProfile.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector, shallowEqual } from 'react-redux';
 
 // CSS
-import { Button } from 'antd';
 import * as s from './MemberProfile.style';
 
-// Functions
-import { checkPagePosition } from '../../functions/checkPagePosition';
+// Components
+import PageTransition from '../../components/PageTransition';
+import BackButton from '../../components/BackButton';
+import MemberProfileAssociation from '../../components/MemberProfileAssociation';
+import MemberProfileValidity from '../../components/MemberProfileValidity';
+import MemberProfileCourses from '../../components/MemberProfileCourses';
 
 const MemberProfile = ({ page, moveToPage }) => {
-	const position = checkPagePosition({
-		actual_page: 3,
-		selected_page: page,
-	});
+	const selected_member_id = useSelector(store => store
+		.memberships.selected_member_id, shallowEqual);
+	const by_id = useSelector(store => store
+		.memberships.by_id, shallowEqual) || {};
+	const member_info = by_id[selected_member_id] || {};
+
+	const nome = member_info.nome || '';
+	const numero = member_info.numero || '';
 
 	return (
-		<s.MemberProfile position={position}>
-			Profile
-			<br /><br />
-			<Button type="primary" onClick={() => moveToPage(2)}>2</Button>
-		</s.MemberProfile>
+		<PageTransition actual_page={3} page={page}>
+			<BackButton
+				moveToPage={moveToPage}
+				page={2}
+			/>
+			<s.PhotoWrapper>
+				<s.Photo />
+			</s.PhotoWrapper>
+			<s.Name>{nome}</s.Name>
+			<s.Number>#{numero}</s.Number>
+			<s.InfoBlock>
+				<MemberProfileAssociation />
+			</s.InfoBlock>
+			<s.InfoBlock>
+				<MemberProfileValidity
+					member_info={member_info}
+				/>
+			</s.InfoBlock>
+			<s.InfoBlock>
+				<MemberProfileCourses
+					member_info={member_info}
+				/>
+			</s.InfoBlock>
+		</PageTransition>
 	);
 };
 
--- src/pages/MemberProfile/MemberProfile.style.js
+++ src/pages/MemberProfile/MemberProfile.style.js
@@ -1,22 +1,46 @@
-import styled, { css } from 'styled-components';
-import theme from '../../styles/theme';
-import { transition } from '../../styles/mixins';
+import styled from 'styled-components';
+import { borderRadius } from '../../styles/mixins';
 
-export const MemberProfile = styled.div`
-	position: absolute;
-	top: 0;
+export const PhotoWrapper = styled.div`
 	width: 100%;
-	${transition(theme.pages_transition)}
+	margin: 0;
+	text-align: center;
+`;
 
-	${props => props.position === 'previous' && css`
-		left: -100%;
-	`}
+export const Photo = styled.div`
+	width: 140px;
+	height: 140px;
+	margin: 40px auto 12px;
+	background: #eee;
+	${borderRadius('140px')}
+`;
 
-	${props => props.position === 'active' && css`
-		left: 0;
-	`}
+export const Name = styled.h1`
+	margin-bottom: 5px;
+	color: #274350;
+	font-weight: bold;
+`;
 
-	${props => props.position === 'next' && css`
-		left: 100%;
-	`}
+export const Number = styled.div`
+	margin-bottom: 0;
+	font-size: 16px;
+`;
+
+export const InfoBlock = styled.div`
+	position: relative;
+	width: 300px;
+	margin: 25px auto;
+	padding: 0 0 0 80px;
+	text-align: left;
+	color: #274350;
+	font-size: 18px;
+	font-weight: bold;
+	line-height: 1.3;
+
+	> label {
+		display: block;
+		color: rgba(0, 0, 0, 0.65);
+		font-size: 16px;
+		font-weight: normal;
+	}
 `;
