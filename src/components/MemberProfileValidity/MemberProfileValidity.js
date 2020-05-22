278b12c7f1365d28d3548acd61c6e3598fba600c
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-22 16:09:25 -0300

    [master] feat: Validity date check

---
+++ src/components/MemberProfileValidity/MemberProfileValidity.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+// FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+// CSS
+import * as s from './MemberProfileValidity.style';
+
+const MemberProfileValidity = ({ member_info = {} }) => {
+	const validade = member_info.validade || '';
+
+	function checkValidity() {
+		const split_values = validade.split('/');
+		const english_format = `${split_values[1]}-${split_values[0]}-01`;
+		return moment().isSameOrBefore(english_format, 'month');
+	}
+
+	if (validade === '') {
+		return null;
+	}
+
+	const is_valid = checkValidity();
+	const icon = is_valid
+		? <FontAwesomeIcon icon={faCheck} />
+		: <FontAwesomeIcon icon={faTimes} />;
+	const validity_view = is_valid
+		? validade
+		: <s.InvalidDate>{validade}</s.InvalidDate>;
+
+	return (
+		<>
+			<s.Status is_valid={is_valid}>
+				{icon}
+			</s.Status>
+			<label>Mensalidade válida até</label>
+			{validity_view}
+		</>
+	);
+};
+
+MemberProfileValidity.propTypes = {
+	member_info: PropTypes.shape({
+		validade: PropTypes.string,
+	}).isRequired,
+};
+
+export default MemberProfileValidity;
---
+++ src/components/MemberProfileValidity/MemberProfileValidity.style.js
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components';
+import { borderRadius } from '../../styles/mixins';
+
+export const Status = styled.div`
+    position: absolute;
+    top: 1px;
+    left: 23px;
+    width: 38px;
+    height: 38px;
+    padding: 9px 0 0 0;
+	background: red;
+	${borderRadius('40px')}
+	color: #fff;
+	text-align: center;
+
+	${props => props.is_valid && css`
+		background: #7cd47c;
+	`}
+`;
+
+export const InvalidDate = styled.span`
+	color: red;
+	text-decoration: line-through;
+`;
