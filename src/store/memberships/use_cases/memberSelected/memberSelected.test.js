b2d77fa3b49bbbb3d346dc1bcde89963270b6332
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-23 18:28:17 -0300

    [master] test: Unit tests for the memberSelected use case

--- src/store/memberships/use_cases/memberSelected/memberSelected.test.js
+++ src/store/memberships/use_cases/memberSelected/memberSelected.test.js
@@ -2,12 +2,14 @@ import memberSelected from './memberSelected';
 import memberships from '../../store';
 
 describe('memberSelected', () => {
-	it('should not break with undefined values', () => {
+	it('should return empty_search error', () => {
 		const call = memberSelected();
-		expect(call).toStrictEqual({});
+		expect(call).toStrictEqual({
+            "error": 'empty_search',
+        });
 	});
 
-	it('should return default store, invalid member id', () => {
+	it('should return user_does_not_exist error', () => {
         const state = {
             ...memberships,
             search: '36',
@@ -20,6 +22,25 @@ describe('memberSelected', () => {
 		expect(result).toStrictEqual({
             ...state,
             selected_member_id: null,
+            error: 'user_does_not_exist',
+        });
+	});
+
+	it('should reset errors', () => {
+        const state = {
+            ...memberships,
+            search: '36',
+            by_id: {
+                '36': { name: 'Name' },
+            },
+            error: 'user_does_not_exist',
+        };
+        const action = { value: 'New' };
+        const result = memberSelected(state, action);
+		expect(result).toStrictEqual({
+            ...state,
+            selected_member_id: '36',
+            error: null,
         });
 	});
 
