96a69ee0ccb2df2887ad46c6dbaf2a59464da323
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-23 15:57:49 -0300

    [master] test: Unit test for memberIdCleared use case

---
+++ src/store/memberships/use_cases/memberIdCleared/memberIdCleared.test.js
@@ -0,0 +1,25 @@
+import memberIdCleared from './memberIdCleared';
+import memberships from '../../store';
+
+describe('memberIdCleared', () => {
+	it('should not break with undefined values', () => {
+		const result = memberIdCleared();
+		const expected = {
+            selected_member_id: null,
+        };
+		expect(result).toStrictEqual(expected);
+	});
+
+	it('should reset selected member id', () => {
+        const state = {
+            ...memberships,
+            selected_member_id: 36,
+        };
+        const action = { value: 'New' };
+        const result = memberIdCleared(state, action);
+		expect(result).toStrictEqual({
+            ...memberships,
+            selected_member_id: null,
+        });
+	});
+});
