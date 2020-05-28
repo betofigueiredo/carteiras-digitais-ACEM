49079b143c28564c2002edddf8c0ca21f4e9ac60
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-27 21:05:14 -0300

    [master] refactor: Clean component

--- src/components/MemberProfileAssociation/MemberProfileAssociation.js
+++ src/components/MemberProfileAssociation/MemberProfileAssociation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
 
 // FontAwesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,27 +7,17 @@ import { faIdCard } from '@fortawesome/free-solid-svg-icons';
 // CSS
 import * as s from './MemberProfileAssociation.style';
 
-const MemberProfileAssociation = () => {
-	const selected_association_id = useSelector(store => store
-		.associations.selected_association_id, shallowEqual);
-	const by_id = useSelector(store => store
-		.associations.by_id, shallowEqual) || {};
-	const association_info = by_id[selected_association_id];
+// Components
+import AssociationSelectedView from '../AssociationSelectedView';
 
-	const invalid_association = association_info === undefined;
-	if (invalid_association) {
-		return null;
-	}
-
-	return (
-		<>
-			<s.Status>
-				<FontAwesomeIcon icon={faIdCard} />
-			</s.Status>
-			<label>Associado à</label>
-			{association_info.sigla}
-		</>
-	);
-};
+const MemberProfileAssociation = () => (
+	<>
+		<s.Status>
+			<FontAwesomeIcon icon={faIdCard} />
+		</s.Status>
+		<label>Associado à</label>
+		<AssociationSelectedView />
+	</>
+);
 
 export default MemberProfileAssociation;
